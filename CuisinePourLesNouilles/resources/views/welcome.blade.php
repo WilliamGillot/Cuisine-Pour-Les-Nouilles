124ca38240f4123735bebd82c743ac235663ad53
Manon <manon.fargues@ynov.com>
2019-11-25 23:41:32 +0100

    menu_deroulant

--- CuisinePourLesNouilles/resources/views/welcome.blade.php
+++ CuisinePourLesNouilles/resources/views/welcome.blade.php
@@ -1,8 +1,3 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -10,63 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Cuisine Pour Les Nouilles</title>
-
+        
+        <link rel="stylesheet" href='styles.css' type="text/css"/>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -88,67 +30,50 @@
                 <div class="title m-b-md">
                     Cuisine pour les Nouilles
                 </div>
-
-                <!-- <nav class="navbar navbar-expand-md navbar-light bg-light">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Recette
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">Apéritif</a>
-                            <a class="dropdown-item" href="#">Entrées</a>
-                            <a class="dropdown-item" href="#">Plats</a>
-                            <a class="dropdown-item" href="#">Desserts</a>
-                            <a class="dropdown-item" href="#">Végétarien</a>
-                            <a class="dropdown-item" href="#">Sans Gluten</a>
-                        </div>
-                    </li> -->
-
-
-                    <div class="container">
-                        <div class="dropdown">
-                            <h1 data-toggle="dropdown"><span class="badge badge-secondary">Recette</span></h1>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="#">Apéritif</a>
-                                <a class="dropdown-item" href="#">Entrées</a>
-                                <a class="dropdown-item" href="#">Plats</a>
-                            </div>
-                        </div>
-                    </div>
-                    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.0/umd/popper.min.js"></script>
-                    <script src="js/bootstrap.min.js"></script>
-                    <script>
-                    $('h1').mouseover(function() {
-                        $(this).dropdown('toggle')
-                    })
-                    </script>
-
-
-                    <!-- <ul class="navbar-nav">
-                        <li class="nav-item active">
-                        <a class="nav-link" href="#">Recette Aléatoire <span class="sr-only">Accueil</span></a>
-                        </li> 
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Articles</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Boutique
-                            </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">Ustensiles</a>
-                            <a class="dropdown-item" href="#">Moules</a>
-                            <a class="dropdown-item" href="#">Livres</a>
-                            <a class="dropdown-item" href="#">Consommables</a>
-                        </div>
-                        </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="#">Table de Conversion</a>
-                        </li>
-                    </ul> -->
-                </nav>
             </div>
+        </div>
+    
+        <div id="menu">
+            <ul>
+                <li>
+                <a href='#'>Recettes</a>
+                <ul>
+                    <li><a href="/aperitifs">Apéritif</a></li>
+                    <li><a href="/entrees">Entrées</a></li>
+                    <li><a href="/plats">Plats</a></li>
+                    <li><a href="/desserts">Desserts</a></li>
+                    <li><a href="/vegetarien">Végétarien</a></li>
+                    <li><a href="/sansgluten">Sans Gluten</a></li>
+                </ul>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                <a href='#'>Recette aléatoire</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                <a href='/articles'>Articles</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                <a href='/boutique'>Boutique</a>
+                <ul>
+                    <li><a href="/ustensiles">Ustensiles</a></li>
+                    <li><a href="/moules">Moules</a></li>
+                    <li><a href="/livres">Livres</a></li>
+                    <li><a href="/consommables">Consommables</a></li>
+                </ul>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                <a href='/tabledeconversion'>Table de conversion</a>
+                </li>
+            </ul>
+        </div>
     </body>
 </html>
 
